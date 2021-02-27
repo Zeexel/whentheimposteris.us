078e0185c407ea4674c7445f684f044dd67d354b
TSG <49595354+AyeTSG@users.noreply.github.com>
2021-02-27 15:55:12 -0500

    implement monster code

---
+++ js/easter_egg_handler.js
@@ -0,0 +1,16 @@
+var monster_pattern = ["m", "o", "n", "s", "t", "e", "r"];
+var monster_current = 0;
+
+var keyHandler = function(event) {
+    if((monster_pattern.indexOf(event.key) < 0) || event.key !== monster_pattern[monster_current]){
+        monster_current = 0;
+        return;
+    }
+
+    monster_current++;
+    if (monster_pattern.length == monster_current) {
+        window.location.replace("https://whentheimposteris.us/monster");   
+    }
+}
+
+document.addEventListener("keydown", keyHandler, false);
